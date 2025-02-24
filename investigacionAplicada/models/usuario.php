023d79a6f9b0e59be5c00de87b91d87eca818e01
Ashley Valdez <VG240979@alumno.udb.edu.sv>
2025-02-24 10:15:28 -0600

    Delete investigacionAplicada directory

--- investigacionAplicada/models/usuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-function obtenerUsuarioPorEmail($conexion, $email) {
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' LIMIT 1";
-    $resultado = $conexion->query($sql);
-    return $resultado->fetch_assoc();
-}
-?>
